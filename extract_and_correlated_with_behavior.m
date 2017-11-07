629e868576fe854769c34327b3f87cf659110c77
kakurk <kurkela@bc.edu>
2017-11-07 15:48:51 -0500

    Make extract*.m file more programmatic

--- extract_and_correlated_with_behavior.m
+++ extract_and_correlated_with_behavior.m
@@ -1,7 +1,7 @@
 % extract ROI estimates and correlate them with behavior
 
 % select a one sample t-test to extract
-one_sample_t_test_spm_mat_file = spm_select;
+one_sample_t_test_spm_mat_file = '/Users/kylekurkela/Desktop/ER_SubEmoMemParMod/OneSampleTs18/0005_Emotional-Trials_vs_Neutral-Trials/SPM.mat';
 
 %% change the path of the SPM.mat file
 
@@ -26,7 +26,19 @@ end
 % `Extract Data --> raw y --> this cluster`. This will create a 
 % number_of_subjects x number_of_voxels matrix called `y` in the workspace.
 
-spm('fMRI')
+% Launch SPM results window, selecting the one_sample_t_test_spm_mat_file
+[SPM, xSPM] = spm_getSPM();
+
+% Hardcoded Voxel Coordinate of left caudate cluster maximum
+XYZmm = [12;4;10];
+
+[~,i] = spm_XYZreg('NearestXYZ',XYZmm,xSPM.XYZmm);
+
+A     = spm_clusters(xSPM.XYZ);
+j     = A == A(i);
+XYZ   = xSPM.XYZ(:,j);
+
+y     = spm_get_data(SPM.xY.VY,XYZ);
 
 %% Take the median of the cluster
 
@@ -39,6 +51,17 @@ left_caudate = median(y, 2); % second dimension, for each row
 % glass brain, right click and select 
 % `Extract Data --> raw y --> this cluster`. This will create a 
 % number_of_subjects x number_of_voxels matrix called `y` in the workspace.
+
+% Hardcoded Voxel Coordinate of right caudate cluster maximum
+XYZmm = [-14;4;8];
+
+[~,i] = spm_XYZreg('NearestXYZ',XYZmm,xSPM.XYZmm);
+
+A     = spm_clusters(xSPM.XYZ);
+j     = A == A(i);
+XYZ   = xSPM.XYZ(:,j);
+
+y     = spm_get_data(SPM.xY.VY,XYZ);
 
 %% Take the median of the cluster
 
